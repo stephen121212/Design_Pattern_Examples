1fcc17aa7beb36ae1e755dae535f76efc041fe9e
stephen121212 <minechart1@gmail.com>
2021-06-23 00:12:30 +0100

    Changes To The Singleton Talk

--- Creational/Singleton/Singleton/src/singleton/SingleObject.java
+++ Creational/Singleton/Singleton/src/singleton/SingleObject.java
@@ -9,20 +9,22 @@ package singleton;
  *
  * @author Stephen
  */
-public class SingleObject {
-    //create an object of SingleObject
-   private static SingleObject instance = new SingleObject();
+public class SingletonObject {
+   //Create an object of SingletonObject that is private therefore classes outside this class definition cannot access it and static so all instance of the class can access this element
+   //If you do not care about overheads or performance better to use an eager instantiation approach as therefore you will not need to worry about Threads potentially creating seperate Singleton Instances
+   //The JVM guarentees that the instance will be created before any thread accesses the static uniqueInstance variable
+   private static SingletonObject uniqueInstance = new SingletonObject();
 
-   //make the constructor private so that this class cannot be
-   //instantiated
-   private SingleObject(){}
+   //Make the constructor private so that this constructor cannot be called outside this class definiton and new instances cannot be instantiated 
+   private SingletonObject(){}
 
-   //Get the only object available
-   public static SingleObject getInstance(){
-      return instance;
+   //Return the Singleton instance to the user when called (Static so all instances of the class can access this method)
+   public static SingletonObject getInstance(){
+      return uniqueInstance;
    }
 
+   //Method that the uniqueInstance contains in order to show the user that both instances are the same in the Test Class (Calls the toString method in order to find where the Object references in memory)
    public void showMessage(){
-      System.out.println("Hello World!");
+      System.out.println(uniqueInstance.toString());
    }
 }
--- Creational/Singleton/Singleton/src/singleton/SingletonPatternDemo.java
+++ Creational/Singleton/Singleton/src/singleton/SingletonPatternDemo.java
@@ -11,15 +11,18 @@ package singleton;
  */
 public class SingletonPatternDemo {
      public static void main(String[] args) {
+      //Creating a Test Method to test the SingletonObject in order to show that the two objects reference the same object in memory
+      
+      //Get the uniqueInstance from the SingletonObject class
+      SingletonObject object = SingletonObject.getInstance();
 
-      //illegal construct
-      //Compile Time Error: The constructor SingleObject() is not visible
-      //SingleObject object = new SingleObject();
-
-      //Get the only object available
-      SingleObject object = SingleObject.getInstance();
-
-      //show the message
+      //Call the showMessage method linked to the Object (Which calls the toString method of the object to show the memory location of the SingletonObject)
       object.showMessage();
+      
+      //Get the uniqueInstance from the SingletonObject class
+      SingletonObject object2 = SingletonObject.getInstance();
+      
+      //Call the showMessage method linked to the Object (Which calls the toString method of the object to show the memory location of the SingletonObject)
+      object2.showMessage();
    }
 }
